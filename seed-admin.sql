1ae00d8c0efc5ad7d30aa582b9ec989cec4953c9
siv3rm-ship-it <siv3r.m@gmail.com>
2025-11-25 18:25:21 +0400

    Delete seed-admin.sql

--- seed-admin.sql
+++
@@ -1,3 +0,0 @@
--- For dev: insert admin user (password should be hashed in real setup)
-INSERT INTO users (id, email, name, role) VALUES ('00000000-0000-0000-0000-000000000001', 'admin@viraltics.test', 'Admin', 'admin')
-ON CONFLICT DO NOTHING;
